c7931f10a548c1128c93a0aa1f6953e9c5081fa5
Augustin Marcin <102642895+amarcin@users.noreply.github.com>
2024-06-12 15:29:05 -0500

    updating tree

--- pages/tree2/script.js
+++ pages/tree2/script.js
@@ -1,149 +1,125 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 5.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v5/
- * ---------------------------------------
- */
-
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+// Create root and chart  
 var root = am5.Root.new("chartdiv");
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
 root.setThemes([
   am5themes_Animated.new(root)
 ]);
 
-var data = {
+var container = root.container.children.push(am5.Container.new(root, {
+  width: am5.percent(100),
+  height: am5.percent(100),
+  layout: root.verticalLayout
+}));
+
+var title = container.children.push(am5.Label.new(root, {
+  text: "Marcin Family Tree",
+  fontSize: 20,
+  x: am5.percent(50),
+  centerX: am5.percent(50)
+}));
+
+var series = container.children.push(am5hierarchy.ForceDirected.new(root, {
+  downDepth: 1,
+  initialDepth: 2,
+  topDepth: 1,
+  valueField: "value",
+  categoryField: "name",
+  childDataField: "children",
+  xField: "x",
+  yField: "y",
+  idField: "id",
+  linkWithField: "linkWith",
+  minRadius: am5.percent(5),
+  maxRadius: am5.percent(10)
+}));
+
+
+series.data.setAll([{
   name: "Root",
   value: 0,
-  children: [
-    {
-      name: "1",
-      linkWith: ["2"],
-      children: [
-        {
-          name: "A",
-          children: [
-            { name: "A1", value: 1 },
-            { name: "A2", value: 1 },
-            { name: "A3", value: 1 },
-            { name: "A4", value: 1 },
-            { name: "A5", value: 1 }
-          ]
-        },
-        {
-          name: "B",
-          children: [
-            { name: "B1", value: 1 },
-            { name: "B2", value: 1 },
-            { name: "B3", value: 1 },
-            { name: "B4", value: 1 },
-            { name: "B5", value: 1 }
-          ]
-        },
-        {
-          name: "C",
-          children: [
-            { name: "C1", value: 1 },
-            { name: "C2", value: 1 },
-            { name: "C3", value: 1 },
-            { name: "C4", value: 1 },
-            { name: "C5", value: 1 }
-          ]
-        }
-      ]
-    },
+  children: [{
+    name: "Paul Marcin",
+    id: "paul",
+    x: am5.percent(40),
+    y: am5.percent(20),
+    children: [{
+      name: "Jim Marcin",
+      linkWith: ["paul", "janice"],  // Connect Jim to both Paul and Janice  
+      children: [{
+        name: "Maret Marcin",
+        value: 90
+      }, {
+        name: "C2A0B2",
+        value: 89
+      }, {
+        name: "C2A0C2",
+        children: [{
+          name: "C2A0C2A3",
+          children: [{
+            name: "C2A0C2A0A4",
+            children: [{
+              name: "C2A0C2A0A00",
+              value: 90
+            }, {
+              name: "C2A0C2A0A01",
+              value: 70
+            }, {
+              name: "C2A0C2A0A02",
+              value: 66
+            }, {
+              name: "C2A0C2A0A03",
+              value: 58
+            }]
+          }, {
+            name: "C2A0C2A0B4",
+            children: [{
+              name: "C2A0C2A0B10",
+              value: 80
+            }, {
+              name: "C2A0C2A0B11",
+              value: 40
+            }]
+          }]
+        }, {
+          name: "C2A0C2B3",
+          value: 44
+        }]
+      }, {
+        name: "C2A0D2",
+        children: [{
+          name: "C2A0D3A3",
+          value: 28
+        }, {
+          name: "C2A0D3B3",
+          value: 14
+        }]
+      }]
+    }, {
+      // Removed "C2B1" and "C2C1" children  
+    }]
+  }, {
+    name: "Janice",
+    id: "janice",
+    linkWith: ["paul"],
+    x: am5.percent(60),
+    y: am5.percent(20),
+    children: [{
+      name: "Child 1",
+      value: 50
+    }, {
+      name: "Child 2",
+      value: 45
+    }, {
+      name: "Child 3",
+      value: 40
+    }]
+  }]
+}]);
 
-    {
-      name: "2",
-      linkWith: ["3"],
-      children: [
-        {
-          name: "D", value: 1
-        },
-        {
-          name: "E", value: 1
-        }
-      ]
-    },
-    {
-      name: "3",
-      children: [
-        {
-          name: "F",
-          children: [
-            { name: "F1", value: 1 },
-            { name: "F2", value: 1 },
-            { name: "F3", value: 1 },
-            { name: "F4", value: 1 },
-            { name: "F5", value: 1 }
-          ]
-        },
-        {
-          name: "H",
-          children: [
-            { name: "H1", value: 1 },
-            { name: "H2", value: 1 },
-            { name: "H3", value: 1 },
-            { name: "H4", value: 1 },
-            { name: "H5", value: 1 }
-          ]
-        },
-        {
-          name: "G",
-          children: [
-            { name: "G1", value: 1 },
-            { name: "G2", value: 1 },
-            { name: "G3", value: 1 },
-            { name: "G4", value: 1 },
-            { name: "G5", value: 1 }
-          ]
-        }
-      ]
-    }
-  ]
-};
-
-// Create wrapper container
-var container = root.container.children.push(
-  am5.Container.new(root, {
-    width: am5.percent(100),
-    height: am5.percent(100),
-    layout: root.verticalLayout
-  })
-);
-
-// Create series
-// https://www.amcharts.com/docs/v5/charts/hierarchy/#Adding
-var series = container.children.push(
-  am5hierarchy.ForceDirected.new(root, {
-    singleBranchOnly: false,
-    downDepth: 1,
-    topDepth: 1,
-    maxRadius: 25,
-    minRadius: 12,
-    valueField: "value",
-    categoryField: "name",
-    childDataField: "children",
-    idField: "name",
-    linkWithStrength: 0.3,
-    linkWithField: "linkWith",
-    manyBodyStrength: -15,
-    centerStrength: 0.5
-  })
-);
-
-series.get("colors").set("step", 2);
-
-series.data.setAll([data]);
 series.set("selectedDataItem", series.dataItems[0]);
 
-// Make stuff animate on load
-series.appear(1000, 100);
+var legend = container.children.push(am5.Legend.new(root, {
+  centerX: am5.percent(90),
+  x: am5.percent(50),
+  layout: root.verticalLayout
+}));  
